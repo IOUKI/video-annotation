85ac2f6b5b374102d60982c488a3291751e002ac
Lan <quw112233456@gmail.com>
2024-04-14 23:33:53 +0800

    commit

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 CORS(app)
 
+# ui = FlaskUI(app, width=1000, height=1000)
+
 @app.get('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)

6d68e56f09614afe1bfb00a65864fe05ec07f845
IOUKI <quw112233456@gmail.com>
2024-07-26 01:54:26 +0800

    add: template layout

---
+++ static/js/layout.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    console.clear()
+    console.log(`build by: %c@LanSan`, 'color: #A6FFFF')
+    console.log(`github: %chttps://github.com/IOUKI`, 'color: #A6FFFF')
+})

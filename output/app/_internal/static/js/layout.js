d8bb4fd40211e1182174bc742b03a82b32f20b8d
Lan <quw112233456@gmail.com>
2024-12-04 00:13:29 +0800

    rm: output file

--- output/app/_internal/static/js/layout.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.clear()
-    console.log(`build by: %c@LanSan`, 'color: #A6FFFF')
-    console.log(`github: %chttps://github.com/IOUKI`, 'color: #A6FFFF')
-})
